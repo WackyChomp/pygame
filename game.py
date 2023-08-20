a6e64cc9a0194d8344a9a0312b693db35ae41312
Wilson Chan <wilsonchan54@gmail.com>
2023-08-20 17:54:01 -0400

    Added image to game screen / added event listener to pressing up and down arrow keys

--- game.py
+++ game.py
@@ -11,13 +11,33 @@ class Game:
 
     self.clock = pygame.time.Clock() # forces game to run at specific frame rate
 
+    self.img = pygame.image.load('data/images/clouds/cloud_1.png')
+    self.img_pos = [160, 260]
+    self.movement = [False, False]
+
   def run(self):
     # Game screen runs in continuous loop with changing data
     while True:
+      self.screen.fill('crimson')      # colors as rgb,text,hex / covers trailing images after movement
+      self.img_pos[1] += (self.movement[1] - self.movement[0]) * 3    # method of adding boolean / multiplication affects speed
+      self.screen.blit(self.img, self.img_pos)
+
       for event in pygame.event.get():
         if event.type == pygame.QUIT:   # registers pressing x button to close screen
           pygame.quit()
           sys.exit()
+        if event.type == pygame.KEYDOWN:    # key pressed
+          if event.key == pygame.K_UP:
+            self.movement[0] = True
+          if event.key == pygame.K_DOWN:
+            self.movement[1] = True
+        if event.type == pygame.KEYUP:      # key release
+          if event.key == pygame.K_UP:
+            self.movement[0] = False
+          if event.key == pygame.K_DOWN:
+            self.movement[1] = False
+
+
 
       pygame.display.update()
       self.clock.tick(60)    # dynamic sleep to maintain 60fps
